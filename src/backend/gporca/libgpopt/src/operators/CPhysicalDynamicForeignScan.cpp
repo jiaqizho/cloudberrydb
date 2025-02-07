greenplum-db/gpdb-archive

Support executing foreign tables from segments in Orca (#15559)
When the mpp_execute option "ANY" is specified, we now will either
execute it on a segment or coordinator depending on the cost.

Previously, when a foreign table was created to be executed with the ANY
location, Orca still only executed it on the coordinator. Now, we treat
it similarly to executing a set-returning function such as
`generate_series()`, and allow it to be executed on either the coordinator
or a segment depending on cost. We add a new table distribution,
Universal, which derives a Universal distribution in Orca just like a
generate_series. It's important to note that this has different behavior
from `all segments`, which treats the data as distributed randomly and
processed as separate data on each segment (for example, accessing a
specific file on each segment).

As an example, if we're using the postgres FDW, for a simple
`select * from pg_foreign_table`, we should perform this on the coordinator
since it would end up being gathered to the coordinator after. However, if
we're performing a join, it's (very) likely to be better to do on a
segment.

Additionally, this PR adds the gp_postgres_fdw regression suite to ICW.

src/backend/gporca/libgpopt/src/operators/CPhysicalDynamicForeignScan.cpp
@@ -15,6 +15,7 @@
 
 #include "gpopt/base/CDistributionSpecRandom.h"
 #include "gpopt/base/CDistributionSpecStrictSingleton.h"
+#include "gpopt/base/CDistributionSpecUniversal.h"
 #include "gpopt/metadata/CName.h"
 #include "gpopt/metadata/CTableDescriptor.h"
 #include "gpopt/operators/CExpressionHandle.h"
@@ -36,25 +37,35 @@ CPhysicalDynamicForeignScan::CPhysicalDynamicForeignScan(
 	ULONG ulOriginOpId, ULONG scan_id, CColRefArray *pdrgpcrOutput,
 	CColRef2dArray *pdrgpdrgpcrParts, IMdIdArray *partition_mdids,
 	ColRefToUlongMapArray *root_col_mapping_per_part, OID foreign_server_oid,
-	BOOL is_coordinator_only)
+	IMDRelation::Ereldistrpolicy exec_location)
 
 
 	: CPhysicalDynamicScan(mp, ptabdesc, ulOriginOpId, pnameAlias, scan_id,
 						   pdrgpcrOutput, pdrgpdrgpcrParts, partition_mdids,
 						   root_col_mapping_per_part),
 	  m_foreign_server_oid(foreign_server_oid),
-	  m_is_coordinator_only(is_coordinator_only)
+	  m_exec_location(exec_location)
 {
+	// we need to overwrite the distribution spec for DynamicForeignGets, as
+	// the partition table can have one distribution, but the distribution for the
+	// ForeignGet can be different. Note this distribution spec mismatch is only
+	// allowed for foreign partitions
 	m_pds->Release();
-	// if this table is coordinator only, use a strict singleton distribution request
-	if (is_coordinator_only)
+	// if this table is coordinator only, set the distribution as strict singleton
+	if (m_exec_location == IMDRelation::EreldistrCoordinatorOnly)
 	{
 		m_pds = GPOS_NEW(mp) CDistributionSpecStrictSingleton(
 			CDistributionSpecSingleton::EstCoordinator);
 	}
-	// otherwise, we want to execute on each segment (but can't assume anything about the distribution)
+	// if the table can be executed on either a segment or coordinator, set it as universal
+	else if (m_exec_location == IMDRelation::EreldistrUniversal)
+	{
+		m_pds = GPOS_NEW(mp) CDistributionSpecUniversal();
+	}
+	// if the distribution is set to "all segments", it is set to a random distribution
 	else
 	{
+		GPOS_ASSERT(m_exec_location == IMDRelation::EreldistrRandom);
 		m_pds = GPOS_NEW(mp) CDistributionSpecRandom();
 	}
 }

jiaqizho/cloudberrydb

src/backend/gporca/libgpopt/src/operators/CPhysicalDynamicForeignScan.cpp
@@ -15,6 +15,7 @@
 
 #include "gpopt/base/CDistributionSpecRandom.h"
 #include "gpopt/base/CDistributionSpecStrictSingleton.h"
+#include "gpopt/base/CDistributionSpecUniversal.h"
 #include "gpopt/metadata/CName.h"
 #include "gpopt/metadata/CTableDescriptor.h"
 #include "gpopt/operators/CExpressionHandle.h"
@@ -36,25 +37,35 @@ CPhysicalDynamicForeignScan::CPhysicalDynamicForeignScan(
 	ULONG ulOriginOpId, ULONG scan_id, CColRefArray *pdrgpcrOutput,
 	CColRef2dArray *pdrgpdrgpcrParts, IMdIdArray *partition_mdids,
 	ColRefToUlongMapArray *root_col_mapping_per_part, OID foreign_server_oid,
-	BOOL is_master_only)
+	IMDRelation::Ereldistrpolicy exec_location)
 
 
 	: CPhysicalDynamicScan(mp, ptabdesc, ulOriginOpId, pnameAlias, scan_id,
 						   pdrgpcrOutput, pdrgpdrgpcrParts, partition_mdids,
 						   root_col_mapping_per_part),
 	  m_foreign_server_oid(foreign_server_oid),
-	  m_is_master_only(is_master_only)
+	  m_exec_location(exec_location)
 {
+	// we need to overwrite the distribution spec for DynamicForeignGets, as
+	// the partition table can have one distribution, but the distribution for the
+	// ForeignGet can be different. Note this distribution spec mismatch is only
+	// allowed for foreign partitions
 	m_pds->Release();
-	// if this table is master only, use a strict singleton distribution request
-	if (is_master_only)
+	// if this table is coordinator only, set the distribution as strict singleton
+	if (m_exec_location == IMDRelation::EreldistrMasterOnly)
 	{
 		m_pds = GPOS_NEW(mp) CDistributionSpecStrictSingleton(
 			CDistributionSpecSingleton::EstMaster);
 	}
-	// otherwise, we want to execute on each segment (but can't assume anything about the distribution)
+	// if the table can be executed on either a segment or coordinator, set it as universal
+	else if (m_exec_location == IMDRelation::EreldistrUniversal)
+	{
+		m_pds = GPOS_NEW(mp) CDistributionSpecUniversal();
+	}
+	// if the distribution is set to "all segments", it is set to a random distribution
 	else
 	{
+		GPOS_ASSERT(m_exec_location == IMDRelation::EreldistrRandom);
 		m_pds = GPOS_NEW(mp) CDistributionSpecRandom();
 	}
 }